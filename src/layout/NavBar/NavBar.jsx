785e09439e2c788bb2978e5b669e991a3be93f82
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-12-20 23:02:36 +0100

    small fixes

--- src/layout/NavBar/NavBar.jsx
+++ src/layout/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <nav className={styles.navbar}>
       <MainLogo />
       <ul className={styles.navbar__list}>
-        <li className={styles.navbar__list__item}>Acceuil</li>
+        <li className={styles.navbar__list__item}>Accueil</li>
         <li className={styles.navbar__list__item}>Profil</li>
         <li className={styles.navbar__list__item}>Réglages</li>
         <li className={styles.navbar__list__item}>Communauté</li>
