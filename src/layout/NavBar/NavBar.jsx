14a19277f2e85b1b6c82b8d3656eb535c795c696
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-12-20 12:32:43 +0100

    init

---
+++ src/layout/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import styles from "./NavBar.module.scss";
+import MainLogo from "../../assets/logo/MainLogo";
+
+const NavBar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <MainLogo />
+      <ul className={styles.navbar__list}>
+        <li className={styles.navbar__list__item}>Acceuil</li>
+        <li className={styles.navbar__list__item}>Profil</li>
+        <li className={styles.navbar__list__item}>Réglages</li>
+        <li className={styles.navbar__list__item}>Communauté</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
