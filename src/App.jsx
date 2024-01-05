b142d80d4259dbbf29a82bef138eb09434fbb3cd
Nicolas Ollier <nicolas@lepetitmartin.com>
2024-01-05 13:17:24 +0100

    add main wrapper styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Layout>
-        <button></button>
+        <button>Hello World</button>
       </Layout>
     </div>
   );
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,13 @@
 import NavBar from "./NavBar/NavBar";
 import SideBar from "./SideBar/SideBar";
+import styles from './Layout.module.scss';
 
 const Layout = ({ children }) => {
   return (
     <>
       <NavBar />
       <SideBar />
-      <main>{children}</main>
+      <main className={styles.main__wrapper}>{children}</main>
     </>
   );
 };
